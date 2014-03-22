685260f4e3bb13b026fbea1e3b67ae8a1284b5b2
jorge13mtb <jorg13mtb@hotmail.com>
2014-03-22 17:54:44 -0600

    Acomodar los elementos de Interfaz grafica

--- InterfazCalculadora.java
+++ InterfazCalculadora.java
@@ -1,12 +1,9 @@
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
@@ -39,13 +36,13 @@ public class InterfazCalculadora
         ventana.setLayout(null);
 
         entrada_operador_1 = new JTextField(20);
-        entrada_operador_1.setSize(100,20);
+        entrada_operador_1.setSize(150,20);
         entrada_operador_1.setLocation(100,100);
         ventana.getContentPane().add(entrada_operador_1);
 
         entrada_operador_2 = new JTextField(20);
-        entrada_operador_2.setSize(100,20);
-        entrada_operador_2.setLocation(400,100);
+        entrada_operador_2.setSize(150,20);
+        entrada_operador_2.setLocation(325,100);
         ventana.getContentPane().add(entrada_operador_2);
 
         resultado = new JTextField(20);
@@ -53,9 +50,9 @@ public class InterfazCalculadora
         resultado.setLocation(600,100);
         ventana.getContentPane().add(resultado);
 
-        operador = new JLabel("Operador");
+        operador = new JLabel("Op");
         operador.setSize(50,20);
-        operador.setLocation(200,100);
+        operador.setLocation(275,100);
         ventana.getContentPane().add(operador);
 
 
@@ -74,7 +71,7 @@ public class InterfazCalculadora
 
         boton_resta = new JButton("-");
         boton_resta.setSize(100,25);
-        boton_resta.setLocation(200,200);
+        boton_resta.setLocation(225,200);
         ventana.getContentPane().add(boton_resta);
 
         boton_resta.addActionListener(new ActionListener() {
@@ -88,7 +85,7 @@ public class InterfazCalculadora
 
         boton_multiplicacion = new JButton("*");
         boton_multiplicacion.setSize(100,25);
-        boton_multiplicacion.setLocation(300,200);
+        boton_multiplicacion.setLocation(350,200);
         ventana.getContentPane().add(boton_multiplicacion);
 
         boton_multiplicacion.addActionListener(new ActionListener() {
@@ -101,7 +98,7 @@ public class InterfazCalculadora
 
         boton_division = new JButton("/");
         boton_division.setSize(100,25);
-        boton_division.setLocation(400,200);
+        boton_division.setLocation(475,200);
         ventana.getContentPane().add(boton_division);
 
         boton_division.addActionListener(new ActionListener() {
@@ -114,13 +111,13 @@ public class InterfazCalculadora
 
         boton_resetear = new JButton("Resetear");
         boton_resetear.setSize(100,25);
-        boton_resetear.setLocation(500,200);
+        boton_resetear.setLocation(600,200);
         ventana.getContentPane().add(boton_resetear);
 
         boton_resetear.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
                 resultado.setText ("");
-                operador.setText ("operador");
+                operador.setText ("Op");
                 entrada_operador_1.setText("");
                 entrada_operador_2.setText("");
         }});
